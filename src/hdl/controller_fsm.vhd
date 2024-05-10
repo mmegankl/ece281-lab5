22d3957385b0c849cae3a3358855ca7a99e75198
mmegankl <141983366+mmegankl@users.noreply.github.com>
2024-05-10 09:10:26 -0600

    Update controller_fsm.vhd

--- src/hdl/controller_fsm.vhd
+++ src/hdl/controller_fsm.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: C3C Megan Leong and C3C Raine Komata
+-- Engineer: 
 -- 
--- Create Date: 04/29/2024 08:15:03 PM
--- Design Name: Lab 5
+-- Create Date: 04/29/2024 08:18:15 PM
+-- Design Name: 
 -- Module Name: controller_fsm - Behavioral
--- Project Name: ALU
+-- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
@@ -15,58 +15,55 @@
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+-- Documentation Statement: None
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
- 
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
- 
+
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
- 
+
 entity controller_fsm is
     port(
         i_reset   : in std_logic;
         i_adv     : in std_logic;
+        i_clk     : in std_logic;
         o_cycle   : out std_logic_vector (3 downto 0)
       );
 end controller_fsm;
- 
+
 architecture Behavioral of controller_fsm is
-    type sm_cycle is (s0,s1,s2,s3);
-    signal f_Q, f_Q_next: sm_cycle;
- 
- 
+    signal f_Q, f_Q_next: std_logic_vector(3 downto 0) := "0001"; 
+    signal w_enable_adv : std_logic;
+
 begin
-f_Q_next <= s0 when ((f_Q = s0) and (i_adv = '0')) else 
-            s0 when ((f_Q = s3) and (i_adv = '0' or i_adv = '1')) else
-            s1 when ((f_Q = s0) and (i_adv = '1')) else
-            s1 when ((f_Q = s1) and (i_adv = '0')) else
-            s2 when ((f_Q = s2) and (i_adv = '0')) else
-            s2 when ((f_Q = s1) and (i_adv = '1')) else
-            s3 when ((f_Q = s2) and (i_adv = '1')) else
+f_Q_next <= "0001" when ((f_Q = "1000") and (i_adv = '1')) else 
+            "1000" when ((f_Q = "0100") and (i_adv = '1')) else 
+            "0100" when ((f_Q = "0010") and (i_adv = '1')) else 
+            "0010" when ((f_Q = "0001") and (i_adv = '1')) else 
             f_Q;
-with f_Q select
-    o_cycle <= "0001" when s0,
-               "0010" when s1,
-               "0100" when s2,
-               "1000" when s3;
+o_cycle <= f_Q;
                
-	register_proc : process (i_adv, i_reset)
+	register_proc : process (i_clk)
        begin
-            -- synchronous reset
+           if(rising_edge(i_clk)) then
            if i_reset = '1' then
-                f_Q <= s0;
-           elsif i_adv = '1' then
-                f_Q <= f_Q_next; 
+                f_Q <= "0001";
+           elsif (i_adv = '1' and w_enable_adv = '1') then
+                f_Q <= f_Q_next;
+                w_enable_adv <= '0';
+           elsif (i_adv = '0') then
+                w_enable_adv <= '1' after 1000 mS;
            end if;
-       end process register_proc;    
- 
+           end if;
+     end process register_proc;    
+
 end Behavioral;
