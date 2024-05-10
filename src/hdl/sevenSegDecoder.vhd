b0d04b3191d8b959a3fcf76ba84cdde3e8cc58f4
mmegankl <141983366+mmegankl@users.noreply.github.com>
2024-05-10 09:07:35 -0600

    Update sevenSegDecoder.vhd

--- src/hdl/sevenSegDecoder.vhd
+++ src/hdl/sevenSegDecoder.vhd
@@ -61,33 +61,46 @@ begin
     c_Sa <= ( not i_D(0) and not i_D(1) and i_D(2) )
         or ( i_D(3) and i_D(2) and not i_D(1) )
         or ( not i_D(3) and not i_D(2) and not i_D(1) and i_D(0) )
-        or ( i_D(3) and not i_D(2) and i_D(1) and i_D(0) );
+        or ( i_D(3) and not i_D(2) and i_D(1) and i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and not i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and i_D(0) );
     c_Sb <= ( i_D(3) and i_D(2) and not i_D(1) and not i_D(0) )
         or ( not i_D(3) and i_D(2) and not i_D(1) and i_D(0) )
         or ( i_D(2) and i_D(1) and not i_D(0) )
-        or ( i_D(3) and i_D(1) and i_D(0) );
+        or ( i_D(3) and i_D(1) and i_D(0) )
+        or (i_D(3) and not i_D(2) and i_D(1) and not i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and i_D(0));
     c_Sc <= ( not i_D(0) and i_D(2) and i_D(3) )
         or ( i_D(1) and i_D(2) and i_D(3) )
-        or ( not i_D(3) and not i_D(2) and i_D(1) and not i_D(0) );
+        or ( not i_D(3) and not i_D(2) and i_D(1) and not i_D(0)) 
+        or (i_D(3) and not i_D(2) and i_D(1) and not i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and i_D(0));
     c_Sd <= ( i_D(0) and not i_D(1) and not i_D(2) )
         or ( i_D(0) and i_D(1) and i_D(2) )
         or ( not i_D(3) and i_D(2) and not i_D(1) and not i_D(0) )
-        or ( i_D(3) and not i_D(2) and i_D(1) and not i_D(0) );
+        or ( i_D(3) and not i_D(2) and i_D(1) and not i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and not i_D(0))
+        or (i_D(3) and not i_D(2) and i_D(1) and i_D(0));
     c_Se <= '1' when ( (i_D = x"1") or
                        (i_D = x"3") or 
                        (i_D = x"4") or
                        (i_D = x"5") or 
                        (i_D = x"7") or 
-                       (i_D = x"9") ) else '0';
+                       (i_D = x"9") or
+                       (i_D = x"A") or
+                       (i_D = x"B")) else '0';
     c_Sf <= '1' when ( (i_D = x"1") or
                        (i_D = x"2") or 
                        (i_D = x"3") or
-                       (i_D = x"7") or 
+                       (i_D = x"7") or
+                       (i_D = x"A") or
+                       (i_D = x"B") or 
                        (i_D = x"C") or 
                        (i_D = x"D") ) else '0';
     c_Sg <= '1' when ( (i_D = x"0") or
                        (i_D = x"1") or 
-                       (i_D = x"7") ) else '0';                                                                 
+                       (i_D = x"7") or 
+                       (i_D = x"B")) else '0';                                                                 
         
 
 
